bab8d7622e47d9def9b68d3e2b115da8c63f53ae
Gustavo Silva dos Santos <gustavosilva.nave@gmail.com>
2016-12-06 23:18:43 -0200

    Trabalho 08 finalizado
    Quer dizer... Eu acho

--- trabalhos/trabalho-08/interpretador.elm
+++ trabalhos/trabalho-08/interpretador.elm
@@ -7,11 +7,14 @@ zero = \ask -> 0
 type Exp = Inteiro Int
          | Var String
          | Add Exp Exp
+         | Sub Exp Exp
          | Mult Exp Exp
          | Div Exp Exp
 
 type Prog = Attr String Exp
           | Seq Prog Prog
+          | If Exp Prog Prog
+          | While Exp Prog
 
 evalExp : Exp -> Env -> Int
 evalExp exp env =
@@ -19,6 +22,7 @@ evalExp exp env =
         Inteiro num    -> num
         Var var        -> (env var)
         Add exp1 exp2  -> (evalExp exp1 env) + (evalExp exp2 env)
+        Sub exp1 exp2  -> (evalExp exp1 env) - (evalExp exp2 env)
         Mult exp1 exp2 -> (evalExp exp1 env) * (evalExp exp2 env)
         Div exp1 exp2  -> (evalExp exp1 env) // (evalExp exp2 env)
 
@@ -31,17 +35,39 @@ evalProg s env =
             let
                 val = (evalExp exp env)
             in
-                \ask -> if ask==var then val else (env ask)
+                \ask -> if ask == var then val else (env ask)
+        If exp prog1 prog2 ->
+            if (evalExp exp env) /= 0 then
+              (evalProg prog1 env)
+            else
+              (evalProg prog2 env)
+        While exp prog ->
+            if (evalExp exp env) /= 0 then
+              (evalProg (While exp prog) (evalProg prog env))
+            else
+              env
 
 lang : Prog -> Int
 lang p = ((evalProg p zero) "ret")
 
-p1 : Prog
-p1 = (Attr "ret" (Add (Inteiro 10) (Inteiro 9)))
+p1: Prog
+p1 = Seq
+          (Attr "x" (Sub (Inteiro 8) (Inteiro 11)))
+          (If (Var "x")
+              (Attr "ret" (Var "x"))
+              (Attr "ret" (Inteiro 100)))
 
 p2 : Prog
 p2 = Seq
+        (Attr "i" (Inteiro 10))
+        (While (Var "i")
+          (Seq
+            (Attr "ret" (Add (Var "ret") (Inteiro 2)))
+            (Attr "i" (Sub (Var "i") (Inteiro 1)))))
+
+p3 : Prog
+p3 = Seq
         (Attr "x"   (Inteiro 11))
         (Attr "ret" (Add (Var "x") (Inteiro 9)))
 
-main = text (toString (lang p1))
+main = text (toString (lang p2))
